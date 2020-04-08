6c1f781adc0f99f09929045ebb5a30ba7c2e14ef
joshuaSmith2021 <joshuasmith6556@gmail.com>
2020-04-08 15:55:31 -0700

    Successfully render page template

--- server.py
+++ server.py
@@ -30,9 +30,6 @@ def bedwars():
 
     display_times = [tools.pretty_time(time) for time in times]
 
-    # Convert display times into the javascript array that will be rendered
-    display_times = ', '.join(map(lambda x: '\'{0}\''.format(x), display_times))
-
     # Now build a dict containing all of the data for charts.js
     datasets = {'kills': [], 'finals': [], 'kdrs': [], 'fkdrs': [], 'wins': [], 'winRate': []}
 
@@ -43,6 +40,12 @@ def bedwars():
 
     for time in times[::-1]:
         dataset = stat_file[time]
+        for username in usernames:
+            # if user does not have an entry for the given time, just repeat the previous time
+            if username not in dataset.keys():
+                for field, lst in datasets.items():
+                    datasets[field][usernames.index(username)]['data'].append(datasets[field][user_index]['data'][-1])
+                
         for user, stats in filter(lambda x: x[0] in usernames, dataset.items()):
             user_index = usernames.index(user)
             bedwars = stats['Bedwars']
@@ -53,4 +56,8 @@ def bedwars():
             datasets['wins'][user_index]['data'].append(bedwars['wins_bedwars'])
             datasets['winRate'][user_index]['data'].append(round(bedwars['wins_bedwars'] / bedwars['games_played_bedwars'] * 1000) / 1000)
 
-    return render_template('bedwars.html', display_times=display_times)
+    for field, lst in datasets.items():
+        datasets[field] = json.dumps(lst[::-1])
+
+    print('Rendering template...')
+    return render_template('bedwars.html', display_times=json.dumps(display_times), datasets=datasets)
