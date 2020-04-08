dab0aa8b8afd12da2fbd76ccfafa04b788be4aa7
joshuaSmith2021 <joshuasmith6556@gmail.com>
2020-04-08 16:42:47 -0700

    Fix score correction errors

--- server.py
+++ server.py
@@ -31,7 +31,7 @@ def bedwars():
     display_times = [tools.pretty_time(time) for time in times]
 
     # Now build a dict containing all of the data for charts.js
-    datasets = {'kills': [], 'finals': [], 'kdrs': [], 'fkdrs': [], 'wins': [], 'winRate': []}
+    datasets = {'kills': [], 'finals': [], 'kdrs': [], 'fkdrs': [], 'wins': [], 'winRate': [], 'kpg': [], 'fkpg': []}
 
     for field in datasets.keys():
         for username in usernames:
@@ -44,7 +44,7 @@ def bedwars():
             # if user does not have an entry for the given time, just repeat the previous time
             if username not in dataset.keys():
                 for field, lst in datasets.items():
-                    datasets[field][usernames.index(username)]['data'].append(datasets[field][user_index]['data'][-1])
+                    datasets[field][usernames.index(username)]['data'].append(datasets[field][usernames.index(username)]['data'][-1])
                 
         for user, stats in filter(lambda x: x[0] in usernames, dataset.items()):
             user_index = usernames.index(user)
@@ -55,8 +55,15 @@ def bedwars():
             datasets['fkdrs'][user_index]['data'].append(round(bedwars['final_kills_bedwars'] / bedwars['final_deaths_bedwars'] * 1000) / 1000)
             datasets['wins'][user_index]['data'].append(bedwars['wins_bedwars'])
             datasets['winRate'][user_index]['data'].append(round(bedwars['wins_bedwars'] / bedwars['games_played_bedwars'] * 1000) / 1000)
+            datasets['kpg'][user_index]['data'].append(round(bedwars['kills_bedwars'] / bedwars['games_played_bedwars'] * 1000) / 1000)
+            datasets['fkpg'][user_index]['data'].append(round(bedwars['final_kills_bedwars'] / bedwars['games_played_bedwars'] * 1000) / 1000)
 
+    colores = ['#ff6384', '#ff9f40', '#ffcd56', '#4bc0c0', '#36c0eb', '#9966ff', '#c9cbcf']
     for field, lst in datasets.items():
+        for i in range(len(lst)):
+            lst[i]['borderColor'] = colores[i]
+
+        # put fields in json format for javascript to read later
         datasets[field] = json.dumps(lst[::-1])
 
     print('Rendering template...')
