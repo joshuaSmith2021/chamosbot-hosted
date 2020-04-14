17e957083c2b6789164a711088f64f3f437be87b
joshuaSmith2021 <joshuasmith6556@gmail.com>
2020-04-14 11:40:49 -0700

    Equip server to show table

--- server.py
+++ server.py
@@ -39,7 +39,9 @@ def bedwars():
     display_times = [tools.pretty_time(time) for time in times]
 
     # Now build a dict containing all of the data for charts.js
-    datasets = {'kills': [], 'finals': [], 'kdrs': [], 'fkdrs': [], 'wins': [], 'winRate': [], 'kpg': [], 'fkpg': []}
+    datasets = {'kills': [], 'finals': [], 'kdrs': [], 'fkdrs': [],
+                'wins': [], 'winRate': [], 'kpg': [], 'fkpg': [],
+                'deaths': [], 'final_deaths': [], 'played': []}
 
     for field in datasets.keys():
         for username in usernames:
@@ -58,13 +60,65 @@ def bedwars():
             user_index = usernames.index(user)
             bedwars = stats['Bedwars']
             datasets['kills'][user_index]['data'].append(bedwars['kills_bedwars'])
+            datasets['deaths'][user_index]['data'].append(bedwars['deaths_bedwars'])
             datasets['finals'][user_index]['data'].append(bedwars['final_kills_bedwars'])
+            datasets['final_deaths'][user_index]['data'].append(bedwars['final_deaths_bedwars'])
             datasets['kdrs'][user_index]['data'].append(round(bedwars['kills_bedwars'] / bedwars['deaths_bedwars'] * 1000) / 1000) 
             datasets['fkdrs'][user_index]['data'].append(round(bedwars['final_kills_bedwars'] / bedwars['final_deaths_bedwars'] * 1000) / 1000)
             datasets['wins'][user_index]['data'].append(bedwars['wins_bedwars'])
+            datasets['played'][user_index]['data'].append(bedwars['games_played_bedwars'])
             datasets['winRate'][user_index]['data'].append(round(bedwars['wins_bedwars'] / bedwars['games_played_bedwars'] * 1000) / 1000)
             datasets['kpg'][user_index]['data'].append(round(bedwars['kills_bedwars'] / bedwars['games_played_bedwars'] * 1000) / 1000)
             datasets['fkpg'][user_index]['data'].append(round(bedwars['final_kills_bedwars'] / bedwars['games_played_bedwars'] * 1000) / 1000)
+
+    performances = []
+    perfKeys = [
+        ('kills', 'Kills'),
+        ('finals', 'Final Kills'),
+        ('!kdr', 'KDR'),
+        ('!fkdr', 'FKDR'),
+        ('wins', 'Wins'),
+        ('!winRate', 'Win Rate')
+        ]
+
+    performances.append([''] + usernames)
+    for i, pair in enumerate(perfKeys):
+        dset, display = pair
+        performances.append([display])
+        for j, username in enumerate(usernames):
+            result = None
+            if dset[0] == '!':
+                try:
+                    if dset[1:] == 'kdr':
+                        kill_data = datasets['kills'][j]['data']
+                        death_data = datasets['deaths'][j]['data']
+                        kills = kill_data[0] - kill_data[-1]
+                        deaths = death_data[0] - death_data[-1]
+                        print(deaths)
+                        result = round(kills / deaths * 1000) / 1000
+                    elif dset[1:] == 'fkdr':
+                        kill_data = datasets['finals'][j]['data']
+                        death_data = datasets['final_deaths'][j]['data']
+                        kills = kill_data[0] - kill_data[-1]
+                        deaths = death_data[0] - death_data[-1]
+                        result = round(kills / deaths * 1000) / 1000
+                    elif dset[1:] == 'winRate':
+                        kill_data = datasets['wins'][j]['data']
+                        death_data = datasets['played'][j]['data']
+                        kills = kill_data[0] - kill_data[-1]
+                        deaths = death_data[0] - death_data[-1]
+                        result = round(kills / deaths * 1000) / 1000
+                except ZeroDivisionError as err:
+                    # A user has had no deaths, final deaths, or games played
+                    #   in the specified time frame, so zerodivision happens.
+                    #   Just return not available... for now...
+                    result = 'Not Available'
+                    #raise(err)
+            else:
+                dataset = datasets[dset][j]['data']
+                result   = dataset[0] - dataset[-1]
+            performances[-1].append(result)
+
 
     colores = ['#ff6384', '#ff9f40', '#ffcd56', '#4bc0c0', '#36c0eb', '#9966ff', '#c9cbcf']
     for field, lst in datasets.items():
@@ -75,5 +129,7 @@ def bedwars():
         # put fields in json format for javascript to read later
         datasets[field] = json.dumps(lst)
 
+    datasets['table'] = json.dumps(performances)
     print('Rendering template...')
+    print(datasets)
     return render_template('bedwars.html', display_times=json.dumps(display_times), datasets=datasets, usernames=json.dumps(usernames))
